19006b6d11a3a64dca2bbc513b8981d33cfed0e1
LINTANG ANDIKA SAPUTRA <77370938+LINTANGDIKA@users.noreply.github.com>
2022-03-05 20:05:06 +0700

    Feat[LD] delete button update profile

--- resources/views/Profile.blade.php
+++ resources/views/Profile.blade.php
@@ -8,19 +8,15 @@
             <img src="{{ url($avatar) }}" class="" alt="..." style="border-radius: 100%">
         </div>
         <div class="detailProfile">
-            <form method="POST">
-                @csrf
                 <div class="mb-3">
                     <label for="name" class="form-label">Your Name : </label>
-                    <input type="text" class="form-control" id="name" name="name" value="{{ $name }}">
+                    <input type="text" class="form-control" id="name" name="name" value="{{ $name }}" readonly>
                 </div>
                 <div class="mb-3">
                     <label for="exampleInputEmail1" class="form-label">Email Address : </label>
                     <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
                         value="{{ $email }}" readonly>
                 </div>
-                <button type="submit" class="btn btn-info">Update Profile</button>
-            </form>
         </div>
     </div>
 </div>
